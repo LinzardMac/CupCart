c1aa42061b5ab2fa498a84076cdf0ba4dd2ff3fc
John Carruthers <johnc@frag-labs.com>
2012-06-30 02:49:28 -0500

    Basic taxonomy listing with awesome view resolution

--- cart-includes/controller/listing.php
+++ cart-includes/controller/listing.php
@@ -9,13 +9,40 @@ class Controller_Listing extends Controller
         
         //  primary filter is most likely to be a category
         $primaryFilterTaxonomy = Taxonomy::get($primaryFilter, "Category");
-        var_dump($primaryFilterTaxonomy);
         if ($primaryFilterTaxonomy == null)
         {
             //  it's not a category, search for other taxonomies
-            
+            $primaryFilterTaxonomy = Taxonomy::get($primaryFilter);
         }
         
-        View::get()->render();
+        if ($primaryFilterTaxonomy == null)
+            throw new HTTP_Exception_404();
+        
+        $taxonomyType = TaxonomyType::getFromCacheByGuid($primaryFilterTaxonomy->typeGuid);
+        
+        $perPage = Hooks::applyFilter("listing_results_per_page", 20);
+        $currentPage = Hooks::applyFilter("listing_page_number", arr::get($_GET,'page',1));
+        if (!is_numeric($currentPage)) $currentPage = 1;
+        
+        $offset = Hooks::applyFilter("listing_offset", ($currentPage - 1) * $perPage);
+        
+        $products = Entity::getByMeta('belongsToTaxonomies', $primaryFilterTaxonomy->guid, $perPage, $offset, 'Product');
+        TPL::setTaxonomy($primaryFilterTaxonomy);
+        TPL::addProductLoop(new Loop($products));
+        
+        //  resolve the view to use
+        $view = '';
+        if (View::exists('listing-'.strtolower($taxonomyType->name).'-'.strtolower($primaryFilterTaxonomy->name)))
+            $view = 'listing-'.strtolower($taxonomyType->name).'-'.strtolower($primaryFilterTaxonomy->name);
+        else if (View::exists('listing-'.strtolower($taxonomyType->name)))
+            $view = 'listing-'.strtolower($taxonomyType->name);
+        else
+        {
+            $view = View::get()->name;
+        }
+        
+        $view = Hooks::applyFilter("listing_".strtolower($taxonomyType->name)."_".strtolower($primaryFilterTaxonomy->name)."_view", $view);
+        
+        View::get($view)->render();
     }
 }
