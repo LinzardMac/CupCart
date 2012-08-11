7a1408b68a35e834c55fdaa390d5b5c4c6fcda9a
John Carruthers <johnc@frag-labs.com>
2012-08-11 02:26:24 -0500

    Moved thumbnail logic to attachment class

--- cart-includes/attachment.php
+++ cart-includes/attachment.php
@@ -49,7 +49,30 @@ class Attachment extends Entity
     public $description;
     
     /**
-     * @var array Array of thumbnails in the format array("WxH:filename", ...) - thumbnails are in the same directory as the original file.
+     * @var array Array of thumbnails in the format array("WxH"=>"filename", ...) - thumbnails are in the same directory as the original file.
     */
-    public $thumbnails;
+    public $thumbnails = array();
+    
+    /**
+     * Gets a thumbnail [Image] if it exists with the given width and height.
+     * @param int $width Thumbnail width
+     * @param int $height Thumbnail height
+     * @return Image
+    */
+    public function getThumbnail($width, $height)
+    {
+	$fileName = '';
+	$thumbnails = $this->thumbnails;
+	if (!is_array($thumbnails))
+	    return null;
+	$dimensions = $width.'x'.$height;
+	if (!array_key_exists($dimensions, $thumbnails))
+	    return null;
+	$fileName = trim($thumbnails[$dimensions]);
+	if ($fileName == '') return null;
+	$fileDir = $this->directoryUri;
+	if (!$this->isLocal)
+	    $fileDir = $this->directoryUrl;
+	return new Image($fileDir.$fileName, $this->directoryUrl.$fileName, $this->isLocal, $width, $height);
+    }
 }
--- cart-includes/product.php
+++ cart-includes/product.php
@@ -43,34 +43,17 @@ class Product extends Entity
 	    $sizes = Core::$activeTheme->getThumbnailSizes();
 	    if (sizeof($sizes) < 1)
 		return null;
-	    $size = $sizes[0];
+	    $size = array_shift($sizes);
 	}
 	$dimensions = Core::$activeTheme->getThumbnailSize($size);
 	if ($dimensions == null) return null;
 	list($width, $height) = $dimensions;
-	$targetDimensions = $width.'x'.$height;
     
 	if ($this->thumbnailAttachment == null)
 	    $this->thumbnailAttachment = Entity::getByGuid($this->thumbnailAttachmentGuid, 'Attachment');
 	if ($this->thumbnailAttachment instanceof Attachment && $this->thumbnailAttachment->type == Attachment::TYPE_IMAGE)
 	{
-	    $fileName = '';
-	    $thumbnails = $this->thumbnailAttachment->thumbnails;
-	    if (!is_array($thumbnails))
-		$thumbnails = array($thumbnails);
-	    foreach($thumbnails as $thumb)
-	    {
-		if ($thumb == null || trim($thumb) == '')
-		    continue;
-		list($dimensions, $name) = explode(":", $thumb, 2);
-		if ($dimensions == $targetDimensions)
-		    $fileName = $name;
-	    }
-	    if ($fileName == '') return null;
-	    $fileDir = $this->thumbnailAttachment->directoryUri;
-	    if (!$this->thumbnailAttachment->isLocal)
-		$fileDir = $this->thumbnailAttachment->directoryUrl;
-	    return new Image($fileDir.$fileName, $this->thumbnailAttachment->directoryUrl.$fileName, $this->thumbnailAttachment->isLocal, $width, $height);
+	    return $this->thumbnailAttachment->getThumbnail($width, $height);
 	}
 	return null;
     }
